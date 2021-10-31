daabb1c58f754fc58bec4f603b4e931c5574c6b6
patchwork-body <gugfug1@gmail.com>
2021-10-31 10:11:27 +0300

    restore DEFAULT_VALUE

--- src/components/workspace-name/index.tsx
+++ src/components/workspace-name/index.tsx
@@ -1,18 +1,25 @@
 import { FormEvent, memo, useCallback, useEffect, useState } from 'react';
 
 const SESSION_STORAGE_KEY = 'workspaceName';
+const DEFAULT_VALUE = 'Enter Your Workspace Name';
 
 export const WorkspaceName = memo(function WorkspaceName() {
   const [value, setValue] = useState('');
 
   const input = useCallback((event: FormEvent<HTMLInputElement>) => {
     const nextValue = event.currentTarget.value;
+    document.title = nextValue;
     sessionStorage.setItem(SESSION_STORAGE_KEY, nextValue);
     setValue(nextValue);
   }, []);
 
+  const restore = useCallback(() => {
+    setValue(DEFAULT_VALUE);
+  }, []);
+
   useEffect(() => {
-    const defaultValue = sessionStorage.getItem(SESSION_STORAGE_KEY) || 'Enter Your Workspace Name';
+    const defaultValue = sessionStorage.getItem(SESSION_STORAGE_KEY) || DEFAULT_VALUE;
+    document.title = defaultValue;
     setValue(defaultValue);
   }, []);
 
@@ -22,6 +29,7 @@ export const WorkspaceName = memo(function WorkspaceName() {
         Workspace Name
         <input
           onInput={input}
+          onBlur={restore}
           className="text-center text-gray-800 outline-none border-2 border-transparent focus:border-blue-400 hover:border-blue-400 rounded-md p-2 w-full font-bold text-xl"
           type="text"
           value={value}
